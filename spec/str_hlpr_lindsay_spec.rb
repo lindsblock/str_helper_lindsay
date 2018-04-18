e7d9fd1ec2f9850b2ac4309ea4edb3d8d0d9ccf8
Lindsay Block <lindsay.block05@gmail.com>
2018-04-18 10:55:43 -0600

    add string helpers

--- spec/str_hlpr_lindsay_spec.rb
+++ spec/str_hlpr_lindsay_spec.rb
@@ -1,9 +1,22 @@
 RSpec.describe StrHlprLindsay do
+  subject { StrHlprLindsay }
+  let(:str) { 'My String' }
+
   it "has a version number" do
     expect(StrHlprLindsay::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'reverses a string' do
+    actual =  subject.reversify(str)
+    expected = 'gnirtS yM'
+    expect(actual).to eq(expected)
   end
+
+  it 'adds spaces' do
+    actual =  subject.spacify(str, 1 )
+    expected = 'M y   S t r i n g'
+    expect(actual).to eq(expected)
+  end
+
+
 end
